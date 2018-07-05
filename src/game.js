7b62947510c3dd77dc6e67cf55bac0c371d0b400
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-05 17:31:01 +0300

    add exercise-unit

--- src/game.js
+++ src/game.js
@@ -2,11 +2,28 @@ import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // esli
 import { cons as consList, l, random, head, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
 
 // @flow
-const run = (player1, player2, cards) => {
+const run = (player1, player2, cards, customRandom) => {
   const iter = (health1, name1, health2, name2, order, log) => {
+    if (health1 <= 0) {
+      return consList(cons(car(head(log)), `${name1} был убит`), log);
+    }
     // BEGIN (write your solution here)
 
     // END
+    const cardName = car(card);
+    const damage = cdr(card)(health2);
+    const newHealth = health2 - damage;
+
+    const message = `Игрок '${name1}' применил '${cardName}'
+      против '${name2}' и нанес урон '${damage}'`;
+    let stats;
+    if (order === 1) {
+      stats = cons(cons(health1, newHealth), message);
+    } else if (order === 2) {
+      stats = cons(cons(newHealth, health1), message);
+    }
+    const newLog = consList(stats, log);
+    return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
   };
 
   const startHealth = 10;
@@ -14,6 +31,6 @@ const run = (player1, player2, cards) => {
   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
 };
 
-export default cards =>
-  (name1, name2) =>
-    run(name1, name2, cards);
+// BEGIN (write your solution here)
+
+// END
