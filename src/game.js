d6712d63e1aa4732f8dfe3a1db6ff6fe16c14a8a
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-05 17:24:59 +0300

    add exercise-unit

---
+++ src/game.js
@@ -0,0 +1,19 @@
+import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
+import { cons as consList, l, random, head, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
+
+// @flow
+const run = (player1, player2, cards) => {
+  const iter = (health1, name1, health2, name2, order, log) => {
+    // BEGIN (write your solution here)
+
+    // END
+  };
+
+  const startHealth = 10;
+  const logItem = cons(cons(startHealth, startHealth), 'Начинаем бой!');
+  return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
+};
+
+export default cards =>
+  (name1, name2) =>
+    run(name1, name2, cards);
