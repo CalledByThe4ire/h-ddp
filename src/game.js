b97ef25184426616fa489c743575646ea2eee80c
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-04 16:11:08 +0300

    add hexlet' solution

--- src/game.js
+++ src/game.js
@@ -14,43 +14,37 @@ import {
 type List = (...args: any) => any | null;
 
 const run = (player1: string, player2: string, cards: List) => {
-  const iter = (health1: number, name1: string, health2: number, name2: string, order: number, log: List) => {
+  const iter = (
+    health1: number,
+    name1: string,
+    health2: number,
+    name2: string,
+    order: number,
+    log: List,
+  ) => {
     // BEGIN (write your solution here)
-
-    let player1Health: number = health1;
-    let player2Health: number = health2;
-    let message: string = '';
-    let logInfo: List = l();
-
-    if (player1Health <= 0 || player2Health <= 0) {
-      message = `Игрок '${player1Health <= 0 ? name1 : name2}' был убит`;
-      logInfo = cons(cons(player1Health, player2Health), message);
-      return consList(logInfo, log);
+    if (health1 <= 0) {
+      return consList(cons(car(head(log)), `${name1} был убит`), log);
     }
+    const card: List = random(cards);
+    const cardName: string = car(card);
+    const damage: number = cdr(card)();
+    const newHealth: number = health2 - damage;
 
-    const rand: List = random(cards);
-    const cardName: string = car(rand);
-    const cardDamage: number = cdr(rand)();
-
-    order % 2 !== 0
-      ? (player2Health -= cardDamage)
-      : (player1Health -= cardDamage);
-    message = `Игрок '${
-      order % 2 !== 0 ? name1 : name2
-    }' применил '${cardName}' против '${
-      order % 2 !== 0 ? name2 : name1
-    }' и нанес урон '${cardDamage}'`;
-
-    logInfo = cons(cons(player1Health, player2Health), message);
-
-    return iter(
-      player1Health,
-      name1,
-      player2Health,
-      name2,
-      order + 1,
-      consList(logInfo, log),
-    );
+    const message: string = `Игрок '${name1}' применил '${cardName}'
+      против '${name2}' и нанес урон '${damage}'`;
+    let stats: List;
+    // В логе игроки всегда должны быть на своих местах. Первый игрок слева, второй - справа
+    if (order === 1) {
+      stats = cons(cons(health1, newHealth), message);
+    } else if (order === 2) {
+      stats = cons(cons(newHealth, health1), message);
+    }
+    const newLog = consList(stats, log);
+    // Хитрость решения учителя состоит в том, что данные игроков всегда меняются местами. Это видно
+    // по вызову ниже. Параметры первого игрока становятся параметрами второго и наоборот.
+    // Такой подход позволяет упростить логику и всегда считать что атакует игрок номер 1.
+    return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
     // END
   };
 
