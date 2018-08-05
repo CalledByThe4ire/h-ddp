96f13111241e50306fce1fa23bbd91e6e626892c
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-05 15:15:10 +0300

    add solution

--- src/simpleCard.js
+++ src/simpleCard.js
@@ -3,5 +3,10 @@ import { attach, contents } from './type';
 
 // BEGIN (write your solution here)
 // @flow
+export const make = (name, loss) =>
+  attach('SimpleCard', cons(name, loss));
 
+export const getName = self => car(contents(self));
+
+export const damage = self => cdr(contents(self));
 // END
--- src/solution.js
+++ src/solution.js
@@ -1,7 +1,26 @@
-import { cons, car, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
-import { cons as consList, l, random, head, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
-import { getName as getSimpleCardName, damage as simpleCardDamage } from './simpleCard';
-import { getName as getPercentCardName, damage as percentCardDamage } from './percentCard';
+/* eslint-disable no-nested-ternary */
+
+import { cons, car, cdr, toString as pairToString } from 'hexlet-pairs'; // eslint-disable-line
+
+import {
+  cons as consList,
+  l,
+  random,
+  head,
+  reverse,
+  toString as listToString,
+} from 'hexlet-pairs-data'; // eslint-disable-line
+
+import {
+  getName as getSimpleCardName,
+  damage as simpleCardDamage,
+} from './simpleCard';
+
+import {
+  getName as getPercentCardName,
+  damage as percentCardDamage,
+} from './percentCard';
+
 import { typeTag } from './type';
 
 // @flow
@@ -11,7 +30,42 @@ const isPercentCard = card => typeTag(card) === 'PercentCard';
 const run = (player1, player2, cards, customRandom) => {
   const iter = (health1, name1, health2, name2, order, log) => {
     // BEGIN (write your solution here)
+    if (health1 <= 0 || health2 <= 0) {
+      const message = `Игрок '${health1 <= 0 ? name1 : name2}' был убит`;
+      const logInfo = cons(cons(health1, health2), message);
+      return consList(logInfo, log);
+    }
 
+    const card = customRandom(cards);
+    const cardName = isSimpleCard(card)
+      ? getSimpleCardName(card)
+      : getPercentCardName(card);
+
+    const cardDamage = isPercentCard(card)
+      ? order % 2 !== 0
+        ? percentCardDamage(card, health2)
+        : percentCardDamage(card, health1)
+      : simpleCardDamage(card);
+
+    const player1Health = order % 2 === 0 ? health1 - cardDamage : health1;
+    const player2Health = order % 2 !== 0 ? health2 - cardDamage : health2;
+
+    const message = `Игрок '${
+      order % 2 !== 0 ? name1 : name2
+    }' применил '${cardName}' против '${
+      order % 2 !== 0 ? name2 : name1
+    }' и нанес урон '${cardDamage}'`;
+
+    const logInfo = cons(cons(player1Health, player2Health), message);
+
+    return iter(
+      player1Health,
+      name1,
+      player2Health,
+      name2,
+      order + 1,
+      consList(logInfo, log),
+    );
     // END
   };
 
@@ -20,6 +74,5 @@ const run = (player1, player2, cards, customRandom) => {
   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
 };
 
-export default (cards, customRandom = random) =>
-  (name1, name2) =>
-    run(name1, name2, cards, customRandom);
+export default (cards, customRandom = random) => (name1, name2) =>
+  run(name1, name2, cards, customRandom);
