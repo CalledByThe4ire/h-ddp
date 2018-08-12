598ff4c1e7e1861a5863e5b145b36d235b9e0bd4
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-12 16:49:45 +0300

    add solution for 'polymorphism'

--- src/extract.js
+++ src/extract.js
@@ -3,5 +3,10 @@ import { getAttribute, getName } from './tags';
 
 // BEGIN (write your solution here)
 // @flow
-
+export default list =>
+  map((element) => {
+    const tagName = getName(element);
+    const attrName = tagName === 'a' || tagName === 'link' ? 'href' : 'src';
+    return getAttribute(attrName, element);
+  }, list);
 // END
