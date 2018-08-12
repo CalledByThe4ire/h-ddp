44cc656121049b349d4ee6ec30ecde8b42166a21
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-12 17:24:37 +0300

    add solution for 'classes'

--- src/simpleCard.js
+++ src/simpleCard.js
@@ -1,4 +1,13 @@
 // BEGIN (write your solution here)
 // @flow
+export default class SimpleCard {
+  constructor(name, loss) {
+    this.name = name;
+    this.loss = loss;
+  }
 
+  damage() {
+    return this.loss;
+  }
+}
 // END
